83c64f0089233d78e646e08f9d0d61f8b6250423
Rudwolfr <Rudwolfr@users.noreply.github.com>
2018-07-17 12:26:02 -0500

    Update coupon.php
    Traducción

--- catalog/language/es-ES/extension/total/coupon.php
+++ catalog/language/es-ES/extension/total/coupon.php
@@ -1,14 +1,14 @@
 <?php
 // Heading
-$_['heading_title'] = 'Use Coupon Code';
+$_['heading_title'] = 'Usar Código de Cupón';
 
 // Text
-$_['text_coupon']   = 'Coupon (%s)';
-$_['text_success']  = 'Success: Your coupon discount has been applied!';
+$_['text_coupon']   = 'Cupón (%s)';
+$_['text_success']  = 'Éxito: ¡Su cupón de descuento ha sido aplicado!';
 
 // Entry
-$_['entry_coupon']  = 'Enter your coupon here';
+$_['entry_coupon']  = 'Teclee su cupón aquí';
 
 // Error
-$_['error_coupon']  = 'Warning: Coupon is either invalid, expired or reached its usage limit!';
-$_['error_empty']   = 'Warning: Please enter a coupon code!';
+$_['error_coupon']  = 'Advertencia: ¡El Cupón no es válido, caducó o alcanzó el límite de uso!';
+$_['error_empty']   = 'Advertencia: ¡Teclee un código de cupón por favor!';
